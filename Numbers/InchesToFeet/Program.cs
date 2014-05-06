3f64b555686854a0835bd40c0bdad5f61c3503c2
Liquid-610 <jamesbarnes71@gmail.com>
2014-05-06 09:57:37 -0400

    work

---
+++ Numbers/InchesToFeet/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InchesToFeet
+{
+    class Program
+    {
+        
+        public static void InchesToFeet(int a)
+        {
+            Console.WriteLine("{0} Feet and {1} Inches",a/12, a % 12);
+        }
+
+
+       
+
+        static void Main(string[] args)
+        {
+            int inches = 67;
+            //int feet = inches % 12;
+            InchesToFeet(inches);
+            
+            
+
+        }
+    
+    }
+}

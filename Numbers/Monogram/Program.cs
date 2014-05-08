19983fc928cad60d65921eae6071bb11ef0b2b4b
Liquid-610 <jamesbarnes71@gmail.com>
2014-05-08 09:42:42 -0400

    monogrizzam

--- Numbers/Monogram/Program.cs
+++ Numbers/Monogram/Program.cs
@@ -7,17 +7,33 @@ namespace Monogram
 {
     class Program
     {
-       /* public static Char //DisplayMonogram()//f,m,l,fr1,fr2,fr3)
+        public static void ConsoleWriteLineDupe (string let, params object[] parameters)
+        {
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                let = let.Replace("{" + i + "}", parameters[i].ToString());
+            }
+
+            Console.WriteLine(let);
+        }
+
+
+        public static void Monogram(char a, char b, char c, char d, char e, char f)
         {
 
-           // return f;
- 
+           Console.WriteLine("** {0}.{1}.{2}. **",a,b,c);
+
+           Console.WriteLine("** {0}.{1}.{2}. **", d, e, f);
         }
-        */
+
         static void Main(string[] args)
         {
-            char F1 = 'J', M1 = 'D', L1 = 'B', FR1 = 'M', FR2 = 'Q', FR3 = 'T';
-            //DisplayMonogram(F1, M1, L1, FR1, FR2, FR3);
+            char f1 = 'J', m1 = 'D', l1 = 'B', f2 = 'M', m2 = 'Q', l2 = 'T';
+
+            
+            Monogram(f1,m1,l1,f2,m2,l2);
+
         }
     }
 }
+    
